d1230582e98a7a9eb510641f30a6390e2e5b5fac
edwardMM <192666@unsaac.edu.pe>
2023-10-04 21:55:58 -0500

    adding upload images with uploadthing.com

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -25,6 +25,7 @@ import {
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { useEffect, useState } from 'react'
+import FieldUpload from '@/components/file-upload'
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -74,8 +75,23 @@ const InitialModal = () => {
         <Form {...form} >
           <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
             <div className="space-y-8 px-6">
-              <div className="flex items-center jusitfy-center text-center">
-                Todo: Image Upload
+              <div className="flex items-center justify-center text-center">
+                {/* Todo: Image Upload */}
+                <FormField
+                  control={form.control}
+                  name='imageUrl'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FieldUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
               <FormField
                 control={form.control}

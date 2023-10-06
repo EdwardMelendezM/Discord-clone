797c4fe2c0d6681a87cfa96e238ac85c23f15871
edwardMM <192666@unsaac.edu.pe>
2023-10-05 22:41:14 -0500

    adding navigation sidebar, item and action

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -3,6 +3,11 @@ import { redirect } from "next/navigation"
 import { db } from "@/lib/db"
 import { currentProfile } from "@/lib/current-profile"
 import NavigationAction from "./navigation-action"
+import { Separator } from "@/components/ui/separator"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import NavigationItem from "./navitation-item"
+import { ModeToggle } from "../mode-toogle"
+import { UserButton } from "@clerk/nextjs"
 
 export const NavigationSidevar = async () => {
   const profile = await currentProfile()
@@ -24,6 +29,29 @@ export const NavigationSidevar = async () => {
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex-1 w-full">
+        {
+          servers.map((server)=> (
+            <div key={server.id} className="mt-5">
+              <NavigationItem
+                id={server.id}
+                name={server.name}
+                imageUrl={server.imageUrl}
+              />
+            </div>
+          ))
+        }
+      </ScrollArea>
+      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: "h-[48px] w-[48px]"
+          }}
+          />
+      </div>
     </div>
   )
 }

f7440d40150e3e04c5032f933be1a96b2845f29f
edumm <192666@unsaac.edu.pe>
2023-10-21 13:04:53 -0500

    adding chat header basic

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -29,7 +29,7 @@ export const ModalProvider = () => {
         <DeleteServerModal/>
         <LeaveServerModal/>
         <CreateServerModal />
-        <InviteModal />\
+        <InviteModal />
         <EditServerModal />
         <MembersModal />
         <CreateChannelModal />

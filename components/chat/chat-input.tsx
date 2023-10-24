631793a0f0843929e5aa1b29c1d0a720c4ac20fe
edumm <192666@unsaac.edu.pe>
2023-10-23 22:29:03 -0500

    adding emoji picker with funtionality correct

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -16,6 +16,8 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { useModal } from "@/hooks/user-modal.store"
+import { EmojiPicker } from "../emoji-picker"
+import { useRouter } from "next/navigation"
 
 interface ChatInputProps{
   apiUrl: string
@@ -36,6 +38,7 @@ const ChatInput = ({
 }: ChatInputProps) => {
 
   const {onOpen} = useModal()
+  const router = useRouter()
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -53,6 +56,8 @@ const ChatInput = ({
         query
       }) 
       await axios.post(url, values)
+      form.reset()
+      router.refresh()
     } catch (error) {
       
     }
@@ -82,7 +87,9 @@ const ChatInput = ({
                     {...field}
                   />
                   <div className="absolute top-7 right-8">
-                    <Smile />
+                    <EmojiPicker
+                      onChange={(emoji: string)=> field.onChange(`${field.value} ${emoji}`)}
+                    />
                   </div>
                 </div>
               </FormControl>
